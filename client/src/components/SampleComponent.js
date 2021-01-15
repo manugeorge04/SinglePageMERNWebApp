8a84f4e2a6e666eb428643a00bed2e8184827571
manugeorge04 <manugeorge004@gmail.com>
2021-01-15 12:52:52 +0530

    Added frontend react boiler plate

---
+++ client/src/components/SampleComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Sample = () => {
+  return(
+  <div>
+    <h1>Sample Component</h1>
+    <p>This is a sample component</p>    
+    </div>
+  )
+};
+
+export default Sample;
+
+
+
+
