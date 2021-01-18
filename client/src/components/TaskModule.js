b9cbc66fbb46d7c3357c1fe092d22f39b0b6a87c
manugeorge04 <manugeorge004@gmail.com>
2021-01-18 10:30:07 +0530

    debug hooks and state

--- client/src/components/TaskModule.js
+++ client/src/components/TaskModule.js
@@ -16,20 +16,25 @@ const TaskModule = () => {
     items: ['Occasion', '']
   }
 
-  const [location, setLocation] = useState("");
-  const [holidayList, setHolidayList] = useState("");
+  
 
-  {console.log("TM")}
+  const [location, setLocation] = useState(""); 
+  const [holidayList, setHolidayList]  = useState([])
+  
+
+  {console.log("Re-render")}
   useEffect(() => {  
     fetchdetails()  
   }, [location])    
 
   const fetchdetails = () => {
-    console.log("hi")
+    console.log("Inside fetch")
     console.log(location)
     if (location !== ""){
-      axios.get(`/api/find/${location}`).then((res) => {
+      axios.get(`/api/find/${location}`).then((res) => {        
+        console.log(res.data)
         setHolidayList(res.data)
+        console.log("state has been set lets check state")
         console.log(holidayList)
       }).catch((err) => {
         console.log(err)
@@ -39,7 +44,9 @@ const TaskModule = () => {
  
   return(      
     <div>
-    {console.log(location)}      
+    {console.log("Inside jsx")}      
+    {console.log(location)}  
+    {console.log(holidayList)}
     <h1>Holidates</h1>    
     <Dropdown  
       search               
